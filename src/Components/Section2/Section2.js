76bdb4d7bad9b5e5229ddabc74142ca28543566d
Bar Linder <barlinder@Bars-MacBook-Pro.local>
2021-06-06 21:45:20 +0300

    text_update

--- src/Components/Section2/Section2.js
+++ src/Components/Section2/Section2.js
@@ -40,18 +40,21 @@ export default function Section2() {
                 <div className="img-div">
                     <img src={interactive}></img>
                     <h2>Interactive</h2>
-                    <p>Websites don't have to be static, I <br></br>love making pages come to life.</p>
+                    {/* <p>Websites don't have to be static, I <br></br>love making pages come to life.</p> */}
+                    <p>Making pages come alive is what i do <br></br>they don't have to be static you know</p>
                 </div>
                 <div className="img-div">
                     <img src={responsive}></img>
                     <h2>Responsive</h2>
-                    <p>My layouts will work on any <br></br>device, big or small.</p>
+                    {/* <p>My layouts will work on any <br></br>device, big or small.</p> */}
+                    <p>My layouts work on any <br></br> screen no matter the size</p>
                     
                 </div>
                 <div className="img-div">
                     <img src={speed}></img>
                     <h2>interactive</h2>
-                    <p>Fast load times and lag free <br></br>interaction, my highest priority.</p>
+                    {/* <p>Fast load times and lag free <br></br>interaction, my highest priority.</p> */}
+                    <p>Lag free fast loads, I care about interaction</p>
                 </div>
             </div>
             <InView inView={inView} onChange={() => handleChange(this)}>
