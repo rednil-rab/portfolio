47e79954cefefb580cfbe36d07c36277a875b2d7
Bar Linder <barlinder@Bars-MacBook-Pro.local>
2021-06-21 09:27:05 +0300

    css_fix_section_1

--- src/Components/Section3/ItemList.js
+++ src/Components/Section3/ItemList.js
@@ -9,28 +9,28 @@ const list = [
         name: 'Cellcom',
         image: cellcom,
         link: 'https://cellcom-sales.co.il/endyear2020/',
-        text: 'Landing Page'
+        text: 'Landing Page for Cellcom'
 
     },
     {
         name: 'Isracard',
         image: isracard,
         link: 'https://adsil1.com/isracard/1120/tiktak/float/',
-        text: 'Landing Page'
+        text: 'Landing Page for Isracard'
 
     },
     {
         name: 'Susu&Sons',
         image: susu,
         link: 'https://adsil1.com/susu/live/',
-        text: 'Landing Page'
+        text: 'Landing Page for Susu&Sons'
 
     },
     {
         name: 'City Market',
         image: market,
         link: 'https://adsil1.com/market/live/',
-        text: 'Landing Page'
+        text: 'Landing Page for City Market'
 
     },
 ]
