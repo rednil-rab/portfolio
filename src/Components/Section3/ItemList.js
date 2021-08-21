dc11ce26471bb996260b79f8fb4b451d813394c0
Bar Linder <barlinder@Bars-MacBook-Pro.local>
2021-08-21 14:01:18 +0300

    projects_update

--- src/Components/Section3/ItemList.js
+++ src/Components/Section3/ItemList.js
@@ -1,10 +1,24 @@
 ////images////
 import cellcom from './images/c-eoy.png';
-import susu from './images/susu.png';
+import weather from './images/weather.png';
 import isracard from './images/i-tt.png';
-import market from './images/market.png';
+import tv from './images/tv.png';
 
 const list = [
+    {
+        name: 'Weater-App',
+        image: weather,
+        link: 'https://rednil-rab.github.io/weather-app/',
+        text: 'an app showing You the weather in any city in the world written with react and redux'
+
+    },
+    {
+        name: 'TV-Game',
+        image: tv,
+        link: 'https://rednil-rab.github.io/tv-game/',
+        text: 'tv show quiz game written with react and typescript'
+
+    },
     {
         name: 'Cellcom',
         image: cellcom,
@@ -19,19 +33,6 @@ const list = [
         text: 'Landing Page for Isracard'
 
     },
-    {
-        name: 'Susu&Sons',
-        image: susu,
-        link: 'https://adsil1.com/susu/live/',
-        text: 'Landing Page for Susu&Sons'
 
-    },
-    {
-        name: 'City Market',
-        image: market,
-        link: 'https://adsil1.com/market/live/',
-        text: 'Landing Page for City Market'
-
-    },
 ]
 export default list;
